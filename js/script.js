430ee69ea2b6135c1d2e54dcc6db81830abe596f
Muhammad Mulya Salam <mulya.salam@alumni.fh-aachen.de>
2023-11-03 22:09:14 +0100

    bikin e prevent default menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 //Toogle active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
 //Toogle active search menu
